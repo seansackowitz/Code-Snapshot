25b030e3df6719c7c6d01ccf0e95fa5e7eaaac46
seansackowitz <keks49585>
2015-09-17 10:36:59 -0400

    Documented Project.cs

--- Code-Snapshot/Project.cs
+++ Code-Snapshot/Project.cs
@@ -4,13 +4,24 @@ namespace Code_Snapshot
 {
     public static class Project
     {
+        /// <summary>
+        /// The List of all top level items
+        /// </summary>
         public static List<Tracked> Tracked = new List<Tracked>();
 
+        /// <summary>
+        /// Adds a new snapshot folder to the top level of the hierarchy
+        /// </summary>
+        /// <param name="Location">The system path of the file</param>
         public static void LoadProject(string Location)
         {
             Tracked.Add(new SnapshotFolder(Location, null));
         }
 
+        /// <summary>
+        /// Adds a new snapshot file to the top level of the hierarchy
+        /// </summary>
+        /// <param name="Location">The system path of the file</param>
         public static void LoadFile(string Location)
         {
             Tracked.Add(new SnapshotFile(Location, null));
